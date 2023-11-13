5f127cfd332055e8f9128270ce061800830a3f66
Mounitha Vemula <88839594+VEMULA-MOUNITHA@users.noreply.github.com>
2023-11-13 16:44:02 -0600

    Committed files

---
+++ SampleExam/SampleExam/StudentsTVC.swift
@@ -0,0 +1,31 @@
+//
+//  StudentsTVC.swift
+//  SampleExam
+//
+//  Created by Mounitha Vemula on 11/13/23.
+//
+
+import UIKit
+
+class StudentsTVC: UITableViewCell,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
